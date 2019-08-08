78f417a9740c842059a6afa917b6e6a77bb3fb8b
Sophie <sophielamb3@outlook.com>
2019-08-08 17:44:01 +0100

    updated index.js file

--- index.js
+++ index.js
@@ -10,82 +10,12 @@ const Movies = Models.Movie;
 const Users = Models.User;
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
-// const Movies = [{
-//   title: 'Lord of the Rings: The Fellowship of the Ring',
-//   description: 'The Lord of the Rings is a trilogy of films about 9 men on a quest to destroy a powerful ring.',
-//   genre: 'Fantasy-fiction',
-//   director: 'Peter Jackson',
-//   slug: 'lord-of-the-rings',
-//   imageURL: '#',
-// },
-// {
-//   title: 'Harry Potter and the Prisoner of Azkaban',
-//   description: 'In the third film of the series, Harry Potter meets a very important family member.',
-//   genre: 'Fantasy/Drama',
-//   director: 'Alfonso Cuaron',
-//   slug: 'harry-potter-and-the-prisoner-of-azkaban',
-//   imageURL: '#',
-// },
-// {
-//   title: 'Gladiator',
-//   description: 'An ex army general looses everything, but can he rise to the top of the Gladiator circle?',
-//   genre: 'Action',
-//   director: 'Ridley Scott',
-//   slug: 'gladiator',
-//   imageURL: '#',
-// },
-// {
-//   title: '10 Things I Hate About You',
-//   description: 'A 1999 American romantic comedy about a bittersweet romance.',
-//   genre: 'Romance/Teen',
-//   director: 'Gil Junger',
-//   slug: '10-things-i-hate-about-you',
-//   imageURL: '#',
-// },
-// {
-//   title: 'Legally Blonde',
-//   description: 'A comedy following Elle Woods who has it all, but can she prove everyone wrong by becoming a top lawyer?',
-//   genre: 'Comedy/Romance',
-//   director: 'Robert Luketic',
-//   slug: 'legally-blonde',
-//   imageURL: '#',
-// },
-// {
-//   title: 'Paranormal Activity',
-//   description: 'A family haunted by a paranormal being in their home - will they be able to survive?',
-//   genre: 'Horror/Thriller',
-//   director: 'Oren Peli',
-//   slug: 'paranormal-activity',
-//   imageURL: '#',
-// },
-// ];
-//
-//
-// const Users = [{
-//   name: 'Sophie',
-//   username: 'SophieLamb',
-//   email: 'sophie@test.com',
-//   password: 'sophie123',
-//   dateOfBirth: '23/08/1995',
-//   favourites: [],
-// },
-// {
-//   name: 'Elaine',
-//   username: 'ElaineLamb',
-//   email: 'elaine@test.com',
-//   password: 'elaine123',
-//   dateOfBirth: '05/10/1962',
-//   favourites: [],
-// },
-// ];
-// Get Requests
-// returns list of all movies to the user
-// app.get('/movies', (req, res) => {
-//   res.json(Movies);
-// });
+const passport = require('passport');
+require('./passport')
+var auth = require('./auth')(app);
 
 //return list of all movies using mongoose
-app.get('/movies', function(req,res){
+app.get('/movies', passport.authenticate('jwt', {session: false}), function(req,res){
   Movies.find()
   .then(function(movies){
     res.status(201).json(movies)
@@ -96,15 +26,7 @@ app.get('/movies', function(req,res){
   });
 });
 
-//* OLD CODE*//
-// app.get('/movies/:title', (req, res) => {
-//   // console.log(req.params.title);
-//   const { title } = req.params;
-//   // console.log(req.query.title);
-//   const movies = Movies.filter(movie => movie.slug === title.toLowerCase());
-//   console.log(movies);
-//   res.json(movies);
-// });
+
 
 // get movies by title using mongoose
 app.get('/movies/:Title', function (req,res) {
@@ -118,16 +40,7 @@ app.get('/movies/:Title', function (req,res) {
   });
 });
 
-// * OLD CODE * //
-// // data about genre by title
-// app.get('/movies/:title/genre', (req, res) => {
-//   const selectedMovie = Movies.find(movie => movie.title.toLowerCase() === req.params.title.toLowerCase());
-//   if (selectedMovie) {
-//     res.status(200).send(`The genre of ${selectedMovie.title} is ${selectedMovie.genre}`);
-//   } else {
-//     res.status(404).send(`Movie of the title ${req.params.title}was not found! Please try again.`);
-//   }
-// });
+
 
 // return data about a genre (description) by name (eg Thriller) using mongoose
 app.get('/movies/genre/:Genre', function(req,res) {
@@ -141,11 +54,7 @@ app.get('/movies/genre/:Genre', function(req,res) {
   });
 });
 
-// * OLD CODE * //
-// // info about director by name
-// app.get('/movies/:director', (req, res) => {
-//   res.send({data: 'return the directors DOB, bio and description.'});
-// });
+
 
 //Return data about directors
 app.get('/movies/director/:Director', function(req,res) {
@@ -160,19 +69,9 @@ app.get('/movies/director/:Director', function(req,res) {
 });
 
 
-// Allow new user to register * OLD CODE *
-// app.post('/users/', (req, res) => {
-//   res.send({
-//     name: 'Elaine',
-//     username: 'ElaineLamb',
-//     email: 'elaine@test.com',
-//     password: 'elaine123',
-//     dateOfBirth: '05/10/1962',
-//     favourites: [],
-//   });
-// });
 
-//Add a user
+
+//Add a user - allow user to register
 app.post('/users', function(req, res) {
   Users.findOne({ Username : req.body.Username })
   .then(function(user) {
