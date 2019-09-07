3df9c79420db9474400adb43ebb8c35e7da60f94
Sophie <sophielamb3@outlook.com>
2019-09-07 12:45:31 +0100

    added in a back button to main view

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
+  }
 
   // One of the "hooks" available in a React Component
   componentDidMount() {
@@ -19,20 +29,42 @@ export class MainView extends React.Component {
       });
   }
 
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
+  handleBackBtnClick() {
+    this.setState({
+      selectedMovie: null
+    });
+  }
 
   render() {
     // If the state isn't initialized, this will throw on runtime
     // before the data is initially loaded
-    const { movies } = this.state;
+    const { movies, selectedMovie } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view"/>;
 
     return (
      <div className="main-view">
-     { movies.map(movie => (
-       <MovieCard key={movie._id} movie={movie}/>
-     ))}
+     {selectedMovie ? (
+       <MovieView
+          movie={selectedMovie}
+          onClick={() => this.handleBackBtnClick()}
+        />
+     ) : (
+        movies.map(movie => (
+         <MovieCard
+            key={movie._id}
+            movie={movie}
+            onClick={movie => this.onMovieClick(movie)}
+          />
+       ))
+     )}
      </div>
     );
   }
