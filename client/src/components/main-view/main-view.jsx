4de331727ab096d076700412cf95b1b1d1ca5f58
Sophie <sophielamb3@outlook.com>
2019-09-18 20:33:31 +0100

    imported director and genre view, and added routes to both in main-view

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -8,6 +8,8 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { DirectorView } from '../director-view/director-view';
+import { GenreView } from '../genre-view/genre-view';
 
 export class MainView extends React.Component {
 
@@ -112,6 +114,14 @@ register() {
         <div className="main-view">
           <Route exact path="/" render={() => movies.map(m => <MovieCard key={m._id} movie={m}/>)}/>
           <Route path="/movies/:moviesID" render={({match}) => <MovieView movie={movies.find(m => m._id === match.params.movieId)}/>}/>
+          <Route path="/directors/:name" render={({match}) => {
+            if (!movies) return <div className="main-view"/>;
+            return <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director}/>
+          }}/>
+          <Route path="/genre/:name" render={({match}) => {
+            if (!movies) return <div className="main-view"/>;
+            return <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre}/>
+          }}/>
         </div>
       </Router>
     );
