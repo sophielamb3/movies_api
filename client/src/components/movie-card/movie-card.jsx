b53b11be87accbd515dc4d0af813bd8601789004
Sophie <sophielamb3@outlook.com>
2019-09-18 20:33:51 +0100

    imported link from react router and updated button link

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -4,21 +4,25 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './movie-card.scss';
 
+import { Link } from "react-router-dom";
+
 
 export class MovieCard extends React.Component {
   render() {
     // This is given to the <MovieCard/> component by the outer world
     // which, in this case, is `MainView`, as `MainView` is what’s
     // connected to your database via the movies endpoint of your API
-    const { movie, onClick } = this.props;
+    const { movie } = this.props;
 
     return (
-            <Card className="mb-3">
+            <Card style={{width: '16rem' }}>
               <Card.Img variant="top" src={movie.ImagePath} />
               <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
-                <Card.Text numberOfLines={1} >{movie.Description}</Card.Text>
-                <Button  onClick={() => onClick(movie)} variant="primary">Read more</Button>
+                <Card.Text numberoflines={1} >{movie.Description}</Card.Text>
+                <Link to={`/movies/${movie._id}`}>
+                  <Button variant="link">Read more</Button>
+                </Link>
               </Card.Body>
             </Card>
     );
@@ -33,5 +37,5 @@ MovieCard.propTypes = {
     Director: PropTypes.string.isRequired,
     ImageURL: PropTypes.string.isRequired
   }).isRequired,
-  onClick: PropTypes.func.isRequired
+  // onClick: PropTypes.func.isRequired
 };
