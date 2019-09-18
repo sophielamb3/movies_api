028968fc5a0585fab3011175900f0480a7cb1530
Sophie <sophielamb3@outlook.com>
2019-09-18 20:34:17 +0100

    updated registration view

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -12,9 +12,25 @@ export function RegistrationView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    axios.post('https://myflixbysophie.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    })
+    .then(response => {
+      const data = response.data;
+      console.log(data);
+      window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+    })
+    .catch(e => {
+      console.log('error registering the user')
+    });
     console.log(username, password, email, birthday);
     props.onSignedIn(username);
   };
+
+
 
   return (
     <Form>
