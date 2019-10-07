f4a860cdc02d86f278647120abbe172e30e6f653
Sophie <sophielamb3@outlook.com>
2019-10-07 19:57:44 +0100

    Added a logged in action

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,5 +1,6 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_LOGGEDIN = 'SET_LOGGEDIN_USER';
 
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
@@ -7,4 +8,8 @@ export function setMovies(value) {
 
 export function setFilter(value) {
   return { type: SET_FILTER, value };
+}
+
+export function setLoggedinUser(value){
+  return {type: SET_LOGGEDIN, value}
 }
